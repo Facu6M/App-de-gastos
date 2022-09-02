81b77cc68de8c1c3514c3e34d42de1d2f3f250ee
Facundo M <105024960+Facu6M@users.noreply.github.com>
2022-09-02 02:44:29 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("formTask")
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  let formInfo = new FormData(form)
+  let table = document.getElementById("tasks");
+  let row = table.insertRow(-1);
+
+let descripcion = document.getElementById("description").value;
+let monto = document.getElementById("monto").value;
+let categoria = document.getElementById("categoria").value;
+
+if (descripcion,monto,categoria == "") {
+  alert("no dejes espacios vacios");
+} else {
+  let newCell = row.insertCell(0);
+  newCell.textContent = formInfo.get("typeTransaction");
+
+  newCell = row.insertCell(1);
+  newCell.textContent = `${descripcion}`
+
+  newCell = row.insertCell(2);
+  newCell.textContent = `${monto}`
+
+  newCell = row.insertCell(3);
+  newCell.textContent = `${categoria}`
+}
+
+document.getElementById("formTask").reset();
+})
+
+agregar();
