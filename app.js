9e1bdf28fb5ac41a3cec1d5db3d5d3d3010f731a
Facundo M <105024960+Facu6M@users.noreply.github.com>
2022-09-13 18:29:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,73 @@
 const form = document.getElementById("formTask")
 
 form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  let formInfo = new FormData(form)
+e.preventDefault();
+
+let formacion = new FormData(form)
+let transactionObj= converFormDataToTransactionObj(formacion);
+
+if(localStorage.getItem('transactionData') === null) {
+  let transactionArray = [];
+  transactionArray.push(transactionObj);
+  localStorage.setItem('transactionData', JSON.stringify(transactionArray));
+} else {
+  let transactionArray = JSON.parse(localStorage.getItem('transactionData'));
+  transactionArray.push(transactionObj);
+  localStorage.setItem('transactionData', JSON.stringify(transactionArray));
+}
+
+insertRowInTransactionTable(transactionObj)
+})
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let transactionObjArr = JSON.parse(localStorage.getItem("transactionData"));
+    for (i=0; i < transactionObjArr.length; i++) {
+        insertRowInTransactionTable(transactionObjArr[i])
+     }
+})
+
+function converFormDataToTransactionObj(formacion) {
+  let type = formacion.get("typeTransaction")
+  let descripcion = formacion.get("description")
+  let monto = formacion.get("monto")
+  let categoria = formacion.get("category")
+  return {
+    "type": type,
+    "descripcion": descripcion,
+    "monto": monto,
+    "categoria": categoria
+  }
+}
+
+
+function insertRowInTransactionTable(transactionObj) {
+
   let table = document.getElementById("tasks");
   let row = table.insertRow(-1);
 
-let type = formInfo.get("typeTransaction")
-let descripcion = document.getElementById("description").value;
-let monto = document.getElementById("amount").value;
-let categoria = document.getElementById("category").value;
+    let newCell = row.insertCell(0);
+    newCell.textContent = transactionObj["type"]
 
-if (type,descripcion,monto,categoria == "") {
-  alert("no dejes espacios vacios");
-} else {
-  let newCell = row.insertCell(0);
-  newCell.textContent = `${type}`
+    newCell = row.insertCell(1);
+    newCell.textContent = transactionObj["descripcion"]
 
-  newCell = row.insertCell(1);
-  newCell.textContent = `${descripcion}`
+    newCell = row.insertCell(2);
+    newCell.textContent = transactionObj["monto"]
 
-  newCell = row.insertCell(2);
-  newCell.textContent = `$${monto}`
+    newCell = row.insertCell(3);
+    newCell.textContent = transactionObj["categoria"];
 
-  newCell = row.insertCell(3);
-  newCell.textContent = `${categoria}`
+  form.reset();
 }
 
-document.getElementById("formTask").reset();
-})
+
+
+
+
+
+
+
 
