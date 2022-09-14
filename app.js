1a58c5cbd4497dafe4ed066986fe4073201c2d4d
Facundo M <105024960+Facu6M@users.noreply.github.com>
2022-09-13 21:37:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,79 +5,77 @@ document.addEventListener("DOMContentLoaded", function(){
      }
   })
 
-const form = document.getElementById("formTask")
 
-form.addEventListener("submit", function(e) {
-e.preventDefault();
-let formacion = new FormData(form)
-let transactionObj= converFormDataToTransactionObj(formacion);
+  const form = document.getElementById("formTask")
 
-if(localStorage.getItem('transactionData') === null) {
-  let transactionArray = [];
-  transactionArray.push(transactionObj);
-  localStorage.setItem('transactionData', JSON.stringify(transactionArray));
-} else {
-  let transactionArray = JSON.parse(localStorage.getItem('transactionData'));
-  transactionArray.push(transactionObj);
-  localStorage.setItem('transactionData', JSON.stringify(transactionArray));
-}
+  form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  let formacion = new FormData(form)
+  let transactionObj= converFormDataToTransactionObj(formacion);
 
-insertRowInTransactionTable(transactionObj)
-})
-
-function converFormDataToTransactionObj(formacion) {
-  let type = formacion.get("typeTransaction")
-  let descripcion = formacion.get("description")
-  let monto = formacion.get("monto")
-  let categoria = formacion.get("category")
-  return {
-    "type": type,
-    "descripcion": descripcion,
-    "monto": monto,
-    "categoria": categoria
+  if(localStorage.getItem('transactionData') === null) {
+    let transactionArray = [];
+    transactionArray.push(transactionObj);
+    localStorage.setItem('transactionData', JSON.stringify(transactionArray));
+  } else {
+    let transactionArray = JSON.parse(localStorage.getItem('transactionData'));
+    transactionArray.push(transactionObj);
+    localStorage.setItem('transactionData', JSON.stringify(transactionArray));
   }
-}
 
-function insertRowInTransactionTable(transactionObj) {
+  insertRowInTransactionTable(transactionObj)
+  })
 
-  let table = document.getElementById("tasks");
-  let row = table.insertRow(-1);
-  row.setAttribute("data-transaction-id", transactionObj["transactionId"])
+  function converFormDataToTransactionObj(formacion) {
+    let type = formacion.get("typeTransaction")
+    let descripcion = formacion.get("description")
+    let monto = formacion.get("monto")
+    let categoria = formacion.get("category")
+    return {
+      "type": type,
+      "descripcion": descripcion,
+      "monto": monto,
+      "categoria": categoria
+    }
+  }
 
-    let newCell = row.insertCell(0);
-    newCell.textContent = transactionObj["type"]
 
-    newCell = row.insertCell(1);
-    newCell.textContent = transactionObj["descripcion"]
+  function insertRowInTransactionTable(transactionObj) {
 
-    newCell = row.insertCell(2);
-    newCell.textContent = transactionObj["monto"]
+    let table = document.getElementById("tasks");
+    let row = table.insertRow(-1);
 
-    newCell = row.insertCell(3);
-    newCell.textContent = transactionObj["categoria"];
+      let newCell = row.insertCell(0);
+      newCell.textContent = transactionObj["type"]
 
-    let NewdeleteCell = row.insertCell(4);
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent = "eliminar";
-    NewdeleteCell.appendChild(deleteButton)
+      newCell = row.insertCell(1);
+      newCell.textContent = transactionObj["descripcion"]
 
-  deleteButton.addEventListener("click", function(e) {
-    let transactionRow = e.target.parentNode.parentNode;
-    let transactionId = transactionRow.getAttribute("data-transaction-id")
-    transactionRow.remove();
-    deleteTransactionObj(transactionId)
-    })
+      newCell = row.insertCell(2);
+      newCell.textContent = transactionObj["monto"]
 
-  form.reset();
-}
+      newCell = row.insertCell(3);
+      newCell.textContent = transactionObj["categoria"];
 
-function deleteTransactionObj(transactionId) {
-  let transactionObjArr = JSON.parse(localStorage.getItem("transactionData"));
-  let transactionIndexArray = transactionObjArr.findIndex(element => element.transactionId === transactionId)
-  transactionObjArr.splice(transactionIndexArray, 1)
-  let transactionArrayJSON = JSON.stringify(transactionIndexArray)
-  localStorage.setItem("transactionData", transactionArrayJSON)
-}
+      let NewdeleteCell = row.insertCell(4);
+      let deleteButton = document.createElement("button");
+      deleteButton.textContent = "eliminar";
+      NewdeleteCell.appendChild(deleteButton)
+
+    deleteButton.addEventListener("click", function(e) {
+      e.target.parentNode.parentNode.remove()
+      })
+
+    form.reset();
+  }
+
+
+
+
+
+
+
+
 
 
 
